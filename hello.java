006c63e36d2c47c7d2b8f1decbe563e5e13c238e
minhtien2903 <43950811+minhtien2903@users.noreply.github.com>
2018-10-14 18:32:38 +0700

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("“Hello world I'm Tien");
+        System.out.println("Hello World I am TDTU");
     }
 }
