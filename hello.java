0d6e06c3e46cdbab5e48d2c9f6f32fb2c4e62a28
minhtien2903 <43950811+minhtien2903@users.noreply.github.com>
2018-10-14 18:34:33 +0700

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,14 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("“Hello world I'm Tien");
-    }
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) 
+  {
+    Scanner input = new Scanner (System.in);
+    System.out.print("Input your first name: ");
+    String fname = input.next();
+    System.out.print("Input your last name: ");
+    String lname = input.next();
+    System.out.println();
+    System.out.println("Hello I'm\n"+fname+" "+lname);
+  }
 }
